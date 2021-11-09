7cb747b3c2c5afcfd96b3d11f21db4f06189387a
alecvipa <hebrit_626@hotmail.com>
2021-11-09 08:51:27 -0600

    added nosotros tab

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -17,3 +17,14 @@ document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function(){
   $('.carousel').carousel();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.slider').slider();
+});
