879010ad89555f37fe6cb52b79938993f805a679
alecvipa <hebrit_626@hotmail.com>
2021-11-16 17:56:19 -0600

    finished sites

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.post("/ajax/email", function (request, response) {
     var htmlBody = `<h2>Correo de contacto</h2><p>Nombre: ${request.body.name} </p> <p> Correo electrónico: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p>Mensaje:${request.body.message} </p>`;
     var mail = {
         from: '"Team: Xyncs Web Studio',
-        to: 'hebrit_626@hotmail.com',
+        to: 'contacto@idm-serag.com',
         subject: '¡Alguien ha dejado sus datos en tu sitio web!',
         html: htmlBody
     };
